351ce18f3a158ceb6ca1dbab973317bedfa5ba4c
Micaeli Takara Becca <Mikaelitakara@gmail.com>
2023-08-18 12:30:00 -0300

    FEAT: endpoint Get Post By Id

--- src/database/tables/PostDatabase.ts
+++ src/database/tables/PostDatabase.ts
@@ -1,5 +1,6 @@
-import { PostDB } from "../../models/Posts";
+import { PostDB, PostDBWithCreatorName } from "../../models/Posts";
 import { BaseDatabase } from "../BaseDatabase";
+import { UserDatabase } from "./UserDatabase";
 
 export class PostDatabase extends BaseDatabase {
     public static TABLE_POST = "posts"
@@ -9,5 +10,37 @@ export class PostDatabase extends BaseDatabase {
         await BaseDatabase
             .connection(PostDatabase.TABLE_POST)
             .insert(postDB)
+    }
+
+    public getPostsWithCreatorsName = async (): Promise<PostDBWithCreatorName[]> => {
+        const result: PostDBWithCreatorName[] = await BaseDatabase
+            .connection(PostDatabase.TABLE_POST)
+            .select(
+                `${PostDatabase.TABLE_POST}.id`,
+                `${PostDatabase.TABLE_POST}.creator_id`,
+                `${PostDatabase.TABLE_POST}.content`,
+                `${PostDatabase.TABLE_POST}.likes`,
+                `${PostDatabase.TABLE_POST}.dislikes`,
+                `${PostDatabase.TABLE_POST}.created_at`,
+                `${PostDatabase.TABLE_POST}.updated_at`,
+                `${UserDatabase.TABLE_USER}.name as creator_name`
+            )
+            .join(
+                `${UserDatabase.TABLE_USER}`,
+                `${PostDatabase.TABLE_POST}.creator_id`,
+                "=",
+                `${UserDatabase.TABLE_USER}.id`
+            )
+
+        return result
+    }
+
+    public findPostById = async (id: string): Promise<PostDB | undefined> => {
+        const [result] = await BaseDatabase
+            .connection(PostDatabase.TABLE_POST)
+            .select()
+            .where({ id })
+
+        return result as PostDB | undefined
     }
 }
--- src/database/tables/UserDatabase.ts
+++ src/database/tables/UserDatabase.ts
@@ -2,16 +2,16 @@ import { UserDB } from "../../models/Users";
 import { BaseDatabase } from "../BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
-    public static TABLE_USERS = "users"
+    public static TABLE_USER = "users"
 
     public insertUser = async (userDB: UserDB): Promise<void> => {
         await BaseDatabase
-            .connection(UserDatabase.TABLE_USERS)
+            .connection(UserDatabase.TABLE_USER)
             .insert(userDB)
     }
 
     public findUserByEmail = async (email: string): Promise<UserDB | undefined> => {
-        const [userDB]: Array<UserDB | undefined> = await BaseDatabase.connection(UserDatabase.TABLE_USERS)
+        const [userDB]: Array<UserDB | undefined> = await BaseDatabase.connection(UserDatabase.TABLE_USER)
             .select()
             .where({ email })
             
